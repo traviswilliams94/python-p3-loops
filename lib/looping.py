bf81ebadec5952ecace5e1f8c58610eee9521eb7
Travis <travis.williams94@gmail.com>
2023-07-02 22:31:23 -0600

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i > 0):
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   int_list = [index ** 2 for index in int_list]
+   return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 3:
+            print("Fizz")
+        elif not i % 5:
+            print("Buzz")
+        else:
+            print(i)
+    
